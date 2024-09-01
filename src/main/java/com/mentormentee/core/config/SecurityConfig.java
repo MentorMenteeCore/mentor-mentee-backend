40f9e96ec108eff61ee7a0966d35b89cb5358526
GiyeonChoi <bdcgy22@daum.net>
2024-09-02 01:15:39 +0900

    docs:RDS 연관 Security설정 수정

--- src/main/java/com/mentormentee/core/config/SecurityConfig.java
+++ src/main/java/com/mentormentee/core/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                 // 인증 url 설정
                 .authorizeHttpRequests(authorize -> authorize
                         // 유저 로그인 이전에 이루어지는 요청 (회원가입, 로그인, 이메일 발송, 처음 학과 나열 페이지) 등과 같은 api는 인증하지 않기 위해 permitAll 적용
-                        .requestMatchers("/api/user/sign-up", "/api/user/login", "/api/email/**", "/api/refresh", "/swagger-ui/**", "/v3/api-docs/**", "api/college/*").permitAll()
+                        .requestMatchers("/api/user/sign-up", "/api/user/login", "/api/email/**", "/api/refresh", "/swagger-ui/**", "/v3/api-docs/**", "/api/college/*").permitAll()
                         .anyRequest().authenticated()
 
                 )
